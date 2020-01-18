494bd46885e9d072ada7120fef54dbc80cc240b4
snowyrainkun <vishu10699@gmail.com>
2020-01-18 22:09:09 +0530

    endpoints models

--- genesis/server/server/urls.py
+++ genesis/server/server/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from endpoints.urls import urlpatterns as endpoints_urlpatterns
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += endpoints_urlpatterns
